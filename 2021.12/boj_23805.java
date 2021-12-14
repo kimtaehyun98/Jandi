89daf0012c7f3ffc17d7d3e4d2f17a40b4a8e1f1
kimtaehyun98 <kimtaehyun98@naver.com>
2021-12-14 15:09:54 +0900

    12/14

---
+++ 2021.12/boj_23805.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+    
+        for(int i=0; i<n; i++){
+            for(int j=0; j<5*n; j++){
+                if(j/n == 3) System.out.print(" ");
+                else System.out.print("@");
+            }
+            System.out.println();
+        }
+        
+        for(int i=0; i<3*n; i++){
+            for(int j=0; j<5*n; j++){
+                if(j/n == 1 || j/n == 3) System.out.print(" ");
+                else System.out.print("@");
+            }
+            System.out.println();
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<5*n; j++){
+                if(j/n == 1) System.out.print(" ");
+                else System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+}
