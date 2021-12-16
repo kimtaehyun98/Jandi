69629d34e414eb6df4dba1807ff45421fbc5f1a0
kimtaehyun98 <kimtaehyun98@naver.com>
2021-12-16 13:50:57 +0900

    12/16

---
+++ 2021.12/boj_23808.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+    
+        for(int i=0; i<5*n; i++){
+            for(int j=0; j<5*n; j++){
+                if(i/n != 2  && i/n != 4 && 1 <= j/n && j/n <= 3) System.out.print(" ");
+                else System.out.print("@");
+            }
+            System.out.println();
+        }
+        
+    }
+}
