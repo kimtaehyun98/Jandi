2ed1b583a7371aa0ec46ce3fe8cc1a36518d0e43
kimtaehyun <kimtaehyun98@naver.com>
2022-05-12 16:02:02 +0900

    5/12

---
+++ 2022.5/boj_3046.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int r1, s;
+	cin >> r1 >> s;
+	cout << 2 * s - r1 << "\n";
+}
